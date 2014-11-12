c672ec9f1d09d864c37a8c03375a7d6e95561bf1
Ronak Gajrawala <usandfriends@gmail.com>
2014-11-11 16:05:45 -0800

    Add user routes.

--- server/routes/api_manager.js
+++ server/routes/api_manager.js
@@ -1,7 +1,6 @@
 var api_manager = app.modules.express.Router();
 
 api_manager.use('/project',require('../routes/api_routes/project'));
-//api_manager.use('/user',require('../routes/api_routes/user'));
-
+api_manager.use('/user',require('../routes/api_routes/user'));
 
 module.exports = api_manager;
